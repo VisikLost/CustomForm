a5b9deee2e9ad94fef1f7ef7263268b2ad6e8803
VisikLost <106622121+VisikLost@users.noreply.github.com>
2023-09-06 12:27:17 +0200

    Delete CustomForm directory

--- CustomForm/script.js
+++
@@ -1,8 +0,0 @@
-let Formular = document.querySelector(".myForm")
-
-Formular.addEventListener("submit", function(event){
-    event.preventDefault()
-
-    event.target.elements.usernameInput.value = ""
-    event.target.elements.passwordInput.value = ""
-})
